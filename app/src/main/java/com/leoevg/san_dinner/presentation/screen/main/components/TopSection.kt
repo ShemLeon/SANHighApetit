58d740f499d509a5bffc1cf5643a53c436cbe346
ShemLeon <shemiakinleonid@gmail.com>
2025-11-30 15:17:20 +0200

    current day mode

--- app/src/main/java/com/leoevg/san_dinner/presentation/screen/main/components/TopSection.kt
+++ app/src/main/java/com/leoevg/san_dinner/presentation/screen/main/components/TopSection.kt
@@ -31,8 +31,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.leoevg.san_dinner.presentation.screen.main.MainScreen
-import com.leoevg.san_dinner.ui.theme.Purple40
-import com.leoevg.san_dinner.ui.theme.Purple80
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 @Composable
 fun TopSection() {
@@ -51,10 +52,11 @@ fun TopSection() {
         Surface(
             shape = RoundedCornerShape(50),
             color = Color.White,
-            shadowElevation = 2.dp
+            shadowElevation = 2.dp,
+
         ) {
             Text(
-                text = "Суббота, 29 ноября",
+                text = getFormattedDate(language),
                 modifier = Modifier
                     .padding(
                         horizontal = 16.dp,
@@ -121,6 +123,18 @@ fun TopSection() {
 
 }
 
+@Composable
+fun getFormattedDate(language: String): String {
+    val calendar = Calendar.getInstance()
+    val locale = when (language) {
+        "RU" -> Locale("ru", "RU")
+        "HE" -> Locale("he", "IL")
+        "EN" -> Locale("en", "US")
+        else -> Locale.getDefault()
+    }
+    val dateFormat = SimpleDateFormat("EEEE, d MMMM", locale)
+    return dateFormat.format(calendar.time)
+}
 
 @Preview(showBackground = true)
 @Composable
