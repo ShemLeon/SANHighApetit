ebd95a7c211dc1efc13ee762b7486d598ed68e19
ShemLeon <shemiakinleonid@gmail.com>
2025-12-22 23:56:22 +0200

    тестирование

--- app/src/main/java/com/leoevg/san_dinner/data/googleFormsApi/GoogleFormApi.kt
+++ app/src/main/java/com/leoevg/san_dinner/data/googleFormsApi/GoogleFormApi.kt
@@ -14,3 +14,21 @@ interface GoogleFormApi {
         @Field("entry.1155397463") selectedDishesNames: String
     ): Response<Unit>
 }
+
+/* Оригинальные параметры SAN
+ @POST("forms/d/e/1FAIpQLSdQVbZMwH5_8Qd-3mj6Iplmkq3x-sFhBtn6L_GIHmkkPfNrkA/formResponse")
+    suspend fun submitForm(
+        @Field("entry.1162574950") name: String,
+        @Field("entry.1789283155") workerId: String,
+        @Field("entry.1155397463") selectedDishesNames: String
+ */
+
+/*
+тестовые параметры
+ @POST("forms/u/0/d/e/1FAIpQLSfDPadKqzUgB7XcPaXmog5y4jC4sE4B21Tvv8GxvNva8_9gsw/formResponse?&submit=Submit")
+    suspend fun submitForm(
+        @Field("entry.2014596361") name: String,
+        @Field("entry.353467567") workerId: String,
+        @Field("entry.184144520") selectedDishesNames: String
+    ): Response<Unit>
+ */
