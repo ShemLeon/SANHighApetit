0abb777d1f85c185fd5983a9c12f16388c51667a
ShemLeon <shemiakinleonid@gmail.com>
2025-12-28 22:49:59 +0200

    min SDK 29 (Android 10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
 
     defaultConfig {
         applicationId = "com.leoevg.san_dinner"
-        minSdk = 33
+        minSdk = 29
         targetSdk = 36
         versionCode = 2
         versionName = "1.1"
